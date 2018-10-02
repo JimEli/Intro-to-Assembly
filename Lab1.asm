9198990bd7c9c6ae3126cd5e06cbb51341151eb5
Jim Eli <31859133+JimEli@users.noreply.github.com>
2018-10-01 18:03:13 -0700

    Update Lab1.asm

--- Lab1.asm
+++ Lab1.asm
@@ -1,4 +1,5 @@
 TITLE MASM Template (main.asm)
+
 ; Description: Lab #1 Hello World Program.
 ; James Eli
 ; 8/29/2018
@@ -8,6 +9,7 @@ TITLE MASM Template (main.asm)
 ; some very basic math on numbers input by user. Checks for
 ; signed 32-bit integer overflow.
 ;
+
 INCLUDE Irvine32.inc
 
 ;---------------------------------------------------
@@ -19,40 +21,46 @@ mOutString MACRO string:REQ
 ENDM
 
 .data
+
 ; Text prompts.
 myMessage    BYTE "Hello, James Eli", 0dh, 0ah, 0
 inPrompt     BYTE "Input an integer (-2147483648 to 2147483647): ", 0
 inFailPrompt BYTE " Try input again!", 0dh, 0ah, 0
 outPrompt    BYTE "Result = ", 0
 errorPrompt  BYTE " Beware! A math overflow occurred.", 0dh, 0ah, 0
+crlf_        BYTE 0dh, 0ah, 0
 
 ; Flag for math overflow
 mathError    DWORD 0 
 OF_ERROR     EQU   1 
 
 .code
+
 ;---------------------------------------------------
 ; Prompt for user input. Get an integer using Irvine's 
-; ReadInt function. Checks carry flag for validity and 
-; repeats if necessary. Receives nothing. Returns 
+; ReadInt function. Checks overflow flag for validity 
+; and repeats if necessary. Receives nothing. Returns 
 ; integer in eax.
 ;---------------------------------------------------
 GetInput PROC
+
 @@:
 	mOutString inPrompt     ; Prompt for numerical input.
 	call ReadInt            ; Read 32-bit integer from console.
-	jno  @F	                ; If carry set, invalid input occured.
+	jno  @F	                ; If overflow set, invalid input occured.
 	mOutString inFailPrompt ; Report bad input.
 	jmp  @B                 ; Do over.
 
 @@:
 	ret                     ; Return valid number in eax.
+
 GetInput ENDP
 
 ;---------------------------------------------------
 ; Program entry point.
 ;---------------------------------------------------
 main PROC
+
 	call Clrscr
 
 	; Hello world.
@@ -69,11 +77,13 @@ main PROC
 	add  ebx, eax            ; Add 1st and 2nd number, result in ebx.
 	jno  @F
 	mov  mathError, OF_ERROR ; Overflow detected.
+
 @@:
 	call GetInput            ; Get 3rd number.
 	sub  ebx, eax            ; Subtract 3rd number, result in ebx.
 	jno  @F
 	mov  mathError, OF_ERROR ; Overflow detected.
+
 @@:
 	mov  eax, ebx            ; Move result to eax.
 	mOutString outPrompt     ; Output result text.
@@ -82,8 +92,10 @@ main PROC
 	cmp  mathError, OF_ERROR ; Math overflow?
 	jne  @F
 	mOutString errorPrompt   ; Math error detected.
+
 @@:
+	mOutString crlf_         ; Output cr/lf.
 	exit
+
 main ENDP
 END main
-
